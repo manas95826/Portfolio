5b6dc3baab5008271b6694aeeca0b69b0b42f37a
Aditya <adityakumarji74@gmail.com>
2023-10-16 02:12:54 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const navigation = document.querySelector(".collapse");
+// const navigation = document.querySelector(".collapse");
 
-const navigationHeight = navigation.offsetHeight;
+// const navigationHeight = navigation.offsetHeight;
 
-document.documentElement.style.setProperty(
-  "--scroll-padding",
-  navigationHeight + "px"
-);
+// document.documentElement.style.setProperty(
+//   "--scroll-padding",
+//   navigationHeight + "px"
+// );
+
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+//         if(top >= offset && top < offset + height) {
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+//             });
+//         };
+//     });
+// };
